7bf841e021c98e9b9f9f76fe5ca686174341bae1
AlexVazquez19 <86645097+AlexVazquez19@users.noreply.github.com>
2023-11-13 17:07:39 -0800

    Begin Exploratory Analysis

--- main.R
+++ main.R
@@ -19,6 +19,19 @@ df <- read.csv("Ad_table.csv")
 # verify data types are what they should be
 sapply(df, class)
 
-# change the 'Runned_Miles' column to integer type
+# change 'Runned_Miles' to integer type
 df <- df %>% mutate(Runned_Miles = na_if(Runned_Miles, ""))
 df$Runned_Miles <- as.integer(df$Runned_Miles)
+
+# change 'Price' to integer type
+df <- df %>% mutate(Price = na_if(Price, "Uknown"))
+df$Price <- as.integer(df$Price)
+
+## Exploratory Analysis ----
+
+# view the vehicle manufacturers are represented in this dataset
+make_counts <- df %>%
+  group_by(Maker) %>%
+  summarize(count = n()) %>%
+  arrange(desc(count))
+make_counts
